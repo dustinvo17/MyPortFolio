54ad70fd4b4a334182c328200be338f7e3e919ee
dustinvo17 <46255218+dustinvo17@users.noreply.github.com>
2019-11-02 20:55:58 -0500

    add scroll effect, finish up

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 init()
 function init(){
- 
+    let scroll = new SmoothScroll('a')
+    let about = document.querySelector('.about')
+    let skills = document.querySelector('.skills')
+    let work = document.querySelector('.project')
+    let footer = document.querySelector('.footer')
+    
+    setTimeout(() => {
+        about.style.display = 'flex'
+        skills.style.display = 'flex'
+        work.style.display = 'block'
+        footer.style.display = 'block'
+    }, 5000);
   
     
 }
